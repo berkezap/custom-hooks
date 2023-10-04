b69a38f08c0615f90d211de1612261377b637e5d
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2023-10-04 21:06:47 +0300

    2

--- custom-hook/src/Sayac.jsx
+++ custom-hook/src/Sayac.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 import { useState } from "react";
+import useCounter from "./useCounter";
 
 export default function Sayac() {
-  const [num, setNum] = useState(0);
-  const increment = () => {
-    setNum((prevNum) => prevNum + 1);
-  };
-  const decrement = () => {
-    setNum((prevNum) => prevNum - 1);
-  };
-  const reset = () => {
-    setNum(0);
-  };
+  const [num, increment, decrement, reset] = useCounter();
   return (
     <div>
       <h2> Sayı = {num}</h2>
