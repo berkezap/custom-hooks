305037a658fe41bf89b3005dbaea0a92df2dff2e
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2023-10-04 13:03:58 +0300

    useTitle

---
+++ custom-hook/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import TitleOne from './TitleOne'
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      <TitleOne/>
+    </div>
+  )
+}
+
+export default App
---
+++ custom-hook/src/useTitle.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+export default function useTitle(num) {
+  useEffect(() => {
+    document.title = `Sayı ${num}`;
+  }, [num]);
+}
